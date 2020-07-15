f9d6c305238846bc8296903dbd02cc369993b4da
Fdelgado14 <67976016+Fdelgado14@users.noreply.github.com>
2020-07-14 23:53:53 -0500

    Add files via upload

---
+++ Calculator-Fernando-Delgado.playground/Contents.swift
@@ -0,0 +1,16 @@
+import UIKit
+
+// top section (inputs)
+let valueOne = 1
+let valueTwo = 2
+//middle section
+let addition = valueOne + valueTwo
+let subtraction =  valueOne + valueTwo
+let multiplication = valueOne + valueTwo
+let division = valueOne + valueTwo
+//bottom section
+print("Addition: " + String(addition))
+print("Subtraction: " + String(subtraction))
+print("Multiplication: " + String(multiplication))
+print("Division: " + String(division))
+
